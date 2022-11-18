43bb3729c792ab2b7f9d15f24a86158b7176c6cd
puffy <klimov-danya.04@mail.ru>
2022-11-18 13:43:44 +0300

    Update tests

--- test/unit_tests.cpp
+++ test/unit_tests.cpp
@@ -12,7 +12,7 @@
 int main() {
     /* ////////////////////////////////////////////////////////////////// */
 
-    auto testSize = linapi::Console::get_size_console_x();
+    auto testSize = linapi::Terminal::get_size_console_x();
 
     if (testSize > 0)  // INFO: Test 1
         std::cout << "Test 1 | " << GREEN << "Passed" << RESET << '[' << testSize << ']' << std::endl;
@@ -21,7 +21,7 @@ int main() {
 
     /* ////////////////////////////////////////////////////////////////// */
 
-    testSize = linapi::Console::get_size_console_y();
+    testSize = linapi::Terminal::get_size_console_y();
 
     if (testSize > 0)  // INFO: Test 2
         std::cout << "Test 2 | " << GREEN << "Passed" << RESET << '[' << testSize << ']' << std::endl;
@@ -90,7 +90,7 @@ int main() {
 
     /* ////////////////////////////////////////////////////////////////// */
 
-    files->mkdir("test");
+    files->make_directory("test");
 
     ls = files->local_search();
 
@@ -112,7 +112,7 @@ int main() {
 
     /* ////////////////////////////////////////////////////////////////// */
 
-    files->mkdir("-v", "test");
+    files->make_directory("-v", "test");
 
     ls = files->local_search();
 
@@ -123,20 +123,169 @@ int main() {
 
     files->remove("-rf", "test");
 
-    /* ////////////////////////////////////////////////////////////////// */  // COPY
+    /* ////////////////////////////////////////////////////////////////// */
 
-    files->copy("-v", "test");
+    std::ofstream file;
+    file.open("test1");
 
+    if (file.is_open())
+        file << "Test words" << std::endl;
+
+    file.close();
+
+    files->copy("test1", "test2");
     ls = files->local_search();
 
-    if (ls == LS_TOUCH)  // INFO: Test 11
+    std::string line1, line2;
+    std::ifstream in1("test1");
+
+    if (in1.is_open())
+        getline(in1, line1);
+
+    in1.close();
+
+    std::ifstream in2("test1");
+
+    if (in2.is_open())
+        getline(in2, line2);
+
+    in2.close();
+
+    if (ls == DEFAULT_COPY && line1 == "Test words" && line2 == "Test words")  // INFO: Test 11
         std::cout << "Test 11 | " << GREEN << "Passed" << RESET << std::endl;
     else
-        std::cout << "Test 11 | " << RED << "Failed" << RESET << '[' << ls << ']' << std::endl;
+        std::cout << "Test 11 | " << RED << "Failed" << RESET << '[' << ls << '|' << line1 << '|' << line2 << ']' <<
+        std::endl;
 
-    files->remove("-rf", "test");
+    files->remove("test1 test2");
 
     /* ////////////////////////////////////////////////////////////////// */
+
+    files->touch("test1 test2");
+    files->make_directory("test");
+    files->copy("-f", "test1 test2", "test");
+
+    ls = files->local_search("test");
+
+    if (ls == PARAM_COPY)  // INFO: Test 12
+        std::cout << "Test 12 | " << GREEN << "Passed" << RESET << std::endl;
+    else
+        std::cout << "Test 12 | " << RED << "Failed" << RESET << '[' << ls << ']' << std::endl;
+
+    files->remove("-rf", "test");
+    files->remove("test1 test2");
+
+    /* ////////////////////////////////////////////////////////////////// */
+
+    file.open("test");
+
+    if (file.is_open())
+        file << "Test words" << std::endl;
+
+    file.close();
+
+    auto cat = files->cat("-e", "test");
+
+    if (cat == "Test words$")  // INFO: Test 13
+        std::cout << "Test 13 | " << GREEN << "Passed" << RESET << std::endl;
+    else
+        std::cout << "Test 13 | " << RED << "Failed" << RESET << '[' << cat << ']' << std::endl;
+
+    files->remove("test");
+
+    /* ////////////////////////////////////////////////////////////////// */
+
+    file.open("test");
+
+    if (file.is_open())
+        file << "Test words" << std::endl;
+
+    file.close();
+
+    std::string more = files->more("test");
+
+    if (more == "Test words")  // INFO: Test 14
+        std::cout << "Test 14 | " << GREEN << "Passed" << RESET << std::endl;
+    else
+        std::cout << "Test 14 | " << RED << "Failed" << RESET << '[' << more << ']' << std::endl;
+
+    files->remove("test");
+
+    /* ////////////////////////////////////////////////////////////////// */
+
+    file.open("test");
+
+    if (file.is_open())
+        file << "Test words" << std::endl;
+
+    file.close();
+
+    std::string head = files->head("test");
+
+    if (head == "Test words")  // INFO: Test 14
+        std::cout << "Test 14 | " << GREEN << "Passed" << RESET << std::endl;
+    else
+        std::cout << "Test 14 | " << RED << "Failed" << RESET << '[' << head << ']' << std::endl;
+
+    files->remove("test");
+
+    /* ////////////////////////////////////////////////////////////////// */
+
+
+    file.open("test");
+
+    if (file.is_open())
+        file << "Test words" << std::endl;
+
+    file.close();
+
+    head = files->head("-q", "test");
+
+    if (head == "Test words")  // INFO: Test 15
+        std::cout << "Test 15 | " << GREEN << "Passed" << RESET << std::endl;
+    else
+        std::cout << "Test 15 | " << RED << "Failed" << RESET << '[' << head << ']' << std::endl;
+
+    files->remove("test");
+
+    /* ////////////////////////////////////////////////////////////////// */
+
+    file.open("test");
+
+    if (file.is_open())
+        file << "Test words" << std::endl;
+
+    file.close();
+
+    std::string tail = files->tail("test");
+
+    if (tail == "Test words")  // INFO: Test 16
+        std::cout << "Test 16 | " << GREEN << "Passed" << RESET << std::endl;
+    else
+        std::cout << "Test 16 | " << RED << "Failed" << RESET << '[' << tail << ']' << std::endl;
+
+    files->remove("test");
+
+    /* ////////////////////////////////////////////////////////////////// */
+
+    file.open("test");
+
+    if (file.is_open())
+        file << "Test words" << std::endl;
+
+    file.close();
+
+    tail = files->tail("-q", "test");
+
+    if (tail == "Test words")  // INFO: Test 17
+        std::cout << "Test 17 | " << GREEN << "Passed" << RESET << std::endl;
+    else
+        std::cout << "Test 17 | " << RED << "Failed" << RESET << '[' << tail << ']' << std::endl;
+
+    files->remove("test");
+
+    /* ////////////////////////////////////////////////////////////////// */
+
 
     return 0;
 }
